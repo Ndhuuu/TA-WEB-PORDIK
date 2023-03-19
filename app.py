7d6bf9469ef8f2233f8813773d29b0596036e5a3
Scytheria <57124040+ScytheOfGrim@users.noreply.github.com>
2023-03-19 08:49:56 +0800

    Update minor di app.py

--- app.py
+++ app.py
@@ -1,35 +1,50 @@
 from flask import Flask, render_template, redirect, url_for, request
-import pymysql
+# import pymysql
+from flask_mysqldb import MySQL
 import config
 
-connection = cursor = None
+# connection = cursor = None
 application = Flask(__name__)
 
-connection = pymysql.connect(host = config.DB_HOST, user = config.DB_USER, password = config.DB_PASSWORD, database = config.DB_NAME)
-cursor = connection.cursor()
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+
+mysql = MySQL(app)
+
 
 @application.route('/')
 def index():
     return render_template('index.html')
 
+
 @application.route('/beranda')
 def beranda():
     return render_template('index.html')
+
 
 @application.route('/tentang')
 def tentang():
     return render_template('tentang.html')
 
+
 @application.route('/layanan')
 def layanan():
     return render_template('layanan.html')
+
 
 @application.route('/login')
 def login():
     return render_template('login.html')
 
+
 @application.route('/home')
 def home():
     return render_template('home.html')
-	
-if __name__ == '__main__':application.run(debug=True)
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
