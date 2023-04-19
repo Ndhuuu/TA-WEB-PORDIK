e84ffc136b8523c7f7ada728c05bd140ca06f39f
Muhammad Randu Diva <ndhu2001@gmail.com>
2023-04-19 08:05:15 +0700

    flask

--- db_pordik.sql
+++ db_pordik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2023 at 07:21 AM
+-- Generation Time: Apr 18, 2023 at 03:53 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,66 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_dataadmin`
+--
+
+CREATE TABLE `tb_dataadmin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `tempat_lahir` varchar(20) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `agama` varchar(10) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_telepon` varchar(15) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `foto` longblob DEFAULT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_dataadmin`
+--
+
+INSERT INTO `tb_dataadmin` (`id`, `username`, `password`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `alamat`, `no_telepon`, `email`, `foto`, `role_id`) VALUES
+(1, 'admin1', 'admin', 'Ferdy Sambo', 'Barru', '1973-02-09', 'Laki-Laki', 'Kristen', 'Jakarta', '089612345', 'sambocs@gmail.com', NULL, 1),
+(2, 'admin2', 'admin', 'Teddy Minahasa', 'Manado', '1970-11-23', 'Laki-Laki', 'Kristen', 'Sulawesi', '0896678910', 'teddycs@gmail.com', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_datamahasiswa`
+--
+
+CREATE TABLE `tb_datamahasiswa` (
+  `id` int(11) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `tempat_lahir` varchar(15) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `agama` varchar(10) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_telepon` varchar(15) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `foto` longblob DEFAULT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_datamahasiswa`
+--
+
+INSERT INTO `tb_datamahasiswa` (`id`, `username`, `password`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `alamat`, `no_telepon`, `email`, `foto`, `role_id`) VALUES
+(1, '212103032', 'mahasiswa', 'Muhammad Randu Diva', 'Sleman', '2001-12-06', 'Laki-Laki', 'Islam', 'Sleman', '089652532796', 'ndhu2001@gmail.com', NULL, 2),
+(2, '212103035', 'mahasiswa', 'Rakhmat Mukti Wibowo', 'Kebumen', '1945-12-01', 'Laki-Laki', 'Islam', 'Kebumen', '085157780118', 'rakhmatcs@gmail.com', NULL, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_role`
 --
 
@@ -40,40 +100,23 @@ INSERT INTO `tb_role` (`id`, `nama`) VALUES
 (1, 'Admin'),
 (2, 'Mahasiswa');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_user`
---
-
-CREATE TABLE `tb_user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(10) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `nama` varchar(25) NOT NULL,
-  `tempat_lahir` varchar(20) NOT NULL,
-  `tanggal_lahir` date NOT NULL,
-  `jenis_kelamin` varchar(10) NOT NULL,
-  `agama` varchar(10) NOT NULL,
-  `alamat` varchar(255) NOT NULL,
-  `no_telepon` varchar(15) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `role_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tb_user`
---
-
-INSERT INTO `tb_user` (`id`, `username`, `password`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `alamat`, `no_telepon`, `email`, `role_id`) VALUES
-(1, 'admin1', 'admin', 'Ferdy Sambo', 'Barru', '1973-02-09', 'Laki-Laki', 'Kristen', 'Jakarta', '089612345', 'sambocs@gmail.com', 1),
-(2, 'admin2', 'admin', 'Teddy Minahasa', 'Manado', '1970-11-23', 'Laki-Laki', 'Kristen', 'Sulawesi', '0896678910', 'teddycs@gmail.com', 1),
-(3, '212103032', 'mahasiswa', 'Muhammad Randu Diva', 'Sleman', '2001-12-06', 'Laki-Laki', 'Islam', 'Yogyakarta', '089652532796', 'ndhu2001@gmail.com', 2),
-(4, '212103035', 'mahasiswa', 'Rakhmat Mukti Wibowo', 'Kebumen', '1945-12-01', 'Laki-Laki', 'Islam', 'Kebumen', '085157780118', 'rakhmatcs@gmail.com', 2);
-
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tb_dataadmin`
+--
+ALTER TABLE `tb_dataadmin`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role` (`role_id`);
+
+--
+-- Indexes for table `tb_datamahasiswa`
+--
+ALTER TABLE `tb_datamahasiswa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_mahasiswa` (`role_id`);
 
 --
 -- Indexes for table `tb_role`
@@ -82,21 +125,36 @@ ALTER TABLE `tb_role`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tb_user`
+-- AUTO_INCREMENT for dumped tables
 --
-ALTER TABLE `tb_user`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `role` (`role_id`);
+
+--
+-- AUTO_INCREMENT for table `tb_dataadmin`
+--
+ALTER TABLE `tb_dataadmin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_datamahasiswa`
+--
+ALTER TABLE `tb_datamahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `tb_user`
+-- Constraints for table `tb_dataadmin`
 --
-ALTER TABLE `tb_user`
-  ADD CONSTRAINT `role` FOREIGN KEY (`role_id`) REFERENCES `tb_role` (`id`);
+ALTER TABLE `tb_dataadmin`
+  ADD CONSTRAINT `role_admin` FOREIGN KEY (`role_id`) REFERENCES `tb_role` (`id`);
+
+--
+-- Constraints for table `tb_datamahasiswa`
+--
+ALTER TABLE `tb_datamahasiswa`
+  ADD CONSTRAINT `role_mahasiswa` FOREIGN KEY (`role_id`) REFERENCES `tb_role` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
